a40245b161d44c4984f0cf8f495b1f78fe6d88f8
ss251 <ss251@uw.edu>
2024-10-14 10:08:52 +0530

    fix: eslint error

--- components/AICreatorStudio.tsx
+++ components/AICreatorStudio.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Card, CardContent } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { PlayerWithControls } from "@/components/PlayerWithControls";
 import Image from 'next/image';
 import { getSrc } from '@livepeer/react/external';
